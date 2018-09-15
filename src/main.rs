6c3147057705bfd9f33c4f9134301ec778f5d991
LothTikar <LothTikar@gmail.com>
2018-09-14 19:15:27 -0500

    Now writing playlists to files

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ extern crate csv;
 
 use std::collections::HashMap;
 use std::env;
+use std::fs::File;
+use std::io::Write;
 
 fn main() {
     let path = env::args()
         .nth(1)
         .expect("No argument provided for playlist path!");
-    let mut playlists_src =
-        csv::Reader::from_path(path + "playlists.csv").expect("Playlist file processing error!");
+    let mut playlists_src = csv::Reader::from_path(format!("{}{}", path, "playlists.csv"))
+        .expect("Playlist file processing error!");
 
     let mut autoplaylist: Vec<String> = Vec::new();
     let mut playlists: HashMap<String, Vec<String>> = HashMap::new();
@@ -22,25 +24,26 @@ fn main() {
             let mut put_on_autoplaylist = false;
 
             for i in 0..row.len() {
+                if row[i].is_empty() {
+                    continue;
+                }
                 match header[i].as_ref() {
                     "Youtube link" => link = row[i].to_string(),
                     "Who dislikes this song?" => dislikes = row[i].to_string(),
-                    "Game/Artist" => {
-                        playlist_names.push(format!(
-                            "{} - {}",
-                            header[i].to_string(),
-                            row[i].to_string()
-                        ));
+                    "Game" => {
+                        for name in row[i].to_string().split(',') {
+                            playlist_names.push(format!("{} - {}", header[i].to_string(), name))
+                        }
                         if row.get(i).unwrap() == "RuneScape" {
                             put_on_autoplaylist = true;
                         }
                     }
                     "Song Name" => (),
-                    _ => playlist_names.push(format!(
-                        "{} - {}",
-                        header[i].to_string(),
-                        row[i].to_string()
-                    )),
+                    _ => {
+                        for name in row[i].to_string().split(',') {
+                            playlist_names.push(format!("{} - {}", header[i].to_string(), name))
+                        }
+                    }
                 }
             }
 
@@ -49,17 +52,24 @@ fn main() {
             }
 
             for name in playlist_names {
-                for i in name.split(',') {
-                    playlists
-                        .entry(i.to_string())
-                        .or_insert(Vec::new())
-                        .push(link.clone());
-                }
+                playlists
+                    .entry(name)
+                    .or_insert(Vec::new())
+                    .push(link.clone());
             }
         }
     }
 
+    let mut file = File::create(format!("{}{}", path, "autoplaylist.txt")).unwrap();
+    for i in autoplaylist {
+        file.write_all((i + "\n").as_bytes());
+    }
+
     for (key, val) in playlists.iter() {
-        println!("key: {} val: {:?}", key, val);
+        println!("{}{}.txt", path, key);
+        let mut file = File::create(format!("{}{}.txt", path, key)).unwrap();
+        for i in val.iter() {
+            file.write_all(format!("{}\n", i).as_bytes());
+        }
     }
 }
